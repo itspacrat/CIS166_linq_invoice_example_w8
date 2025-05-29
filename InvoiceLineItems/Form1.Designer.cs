54b7c5903b8fd5603b2f8f434b7cfa414abaf602
itspacrat <blakemichaelgaynor@gmail.com>
2025-05-29 13:51:43 -0700

    Add project files.

---
+++ InvoiceLineItems/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace InvoiceLineItems
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lvwLineItems = new System.Windows.Forms.ListView();
+            this.chdInvoiceID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chdInvoiceDate = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chdInvoiceTotal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chdProductCode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chdUnitPrice = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chdQuantity = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.chdItemTotal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // lvwLineItems
+            // 
+            this.lvwLineItems.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.chdInvoiceID,
+            this.chdInvoiceDate,
+            this.chdInvoiceTotal,
+            this.chdProductCode,
+            this.chdUnitPrice,
+            this.chdQuantity,
+            this.chdItemTotal});
+            this.lvwLineItems.HideSelection = false;
+            this.lvwLineItems.Location = new System.Drawing.Point(13, 13);
+            this.lvwLineItems.Name = "lvwLineItems";
+            this.lvwLineItems.Size = new System.Drawing.Size(622, 490);
+            this.lvwLineItems.TabIndex = 0;
+            this.lvwLineItems.UseCompatibleStateImageBehavior = false;
+            this.lvwLineItems.View = System.Windows.Forms.View.Details;
+            // 
+            // chdInvoiceID
+            // 
+            this.chdInvoiceID.Text = "InvoiceID";
+            this.chdInvoiceID.Width = 90;
+            // 
+            // chdInvoiceDate
+            // 
+            this.chdInvoiceDate.Text = "InvoiceDate";
+            this.chdInvoiceDate.Width = 90;
+            // 
+            // chdInvoiceTotal
+            // 
+            this.chdInvoiceTotal.Text = "InvoiceTotal";
+            this.chdInvoiceTotal.Width = 90;
+            // 
+            // chdProductCode
+            // 
+            this.chdProductCode.Text = "Product Code";
+            this.chdProductCode.Width = 90;
+            // 
+            // chdUnitPrice
+            // 
+            this.chdUnitPrice.Text = "Unit Price";
+            this.chdUnitPrice.Width = 84;
+            // 
+            // chdQuantity
+            // 
+            this.chdQuantity.Text = "Quantity";
+            this.chdQuantity.Width = 55;
+            // 
+            // chdItemTotal
+            // 
+            this.chdItemTotal.Text = "Item Total";
+            this.chdItemTotal.Width = 95;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(647, 515);
+            this.Controls.Add(this.lvwLineItems);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Invoice Line Items by Invoice Date";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView lvwLineItems;
+        private System.Windows.Forms.ColumnHeader chdInvoiceID;
+        private System.Windows.Forms.ColumnHeader chdProductCode;
+        private System.Windows.Forms.ColumnHeader chdUnitPrice;
+        private System.Windows.Forms.ColumnHeader chdQuantity;
+        private System.Windows.Forms.ColumnHeader chdItemTotal;
+        private System.Windows.Forms.ColumnHeader chdInvoiceDate;
+        private System.Windows.Forms.ColumnHeader chdInvoiceTotal;
+    }
+}
+
---
+++ InvoiceLineItems/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace InvoiceLineItems
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_Load(object sender, EventArgs e) {
+
+            // store all line item + invoice entries in
+            // list references
+            List<LineItem> lineItems = LineItemDB.GetLineItems();
+            List<Invoice> invoices = InvoiceDB.GetInvoices();
+
+            // join these into an IEnumerable before populating the ListView
+            var listViewItems = 
+                from item in lineItems
+                join invoice in invoices
+                on item.InvoiceID equals invoice.InvoiceID
+                orderby invoice.InvoiceDate
+                select new {
+                    item.InvoiceID,
+                    invoice.InvoiceDate,
+                    invoice.InvoiceTotal,
+                    item.ProductCode,
+                    item.UnitPrice,
+                    item.Quantity,
+                    item.ItemTotal
+                };
+
+                // fill the listview
+                foreach (var lineItem in listViewItems) {
+                lvwLineItems.Items.Add(new ListViewItem(new string[] {
+                    lineItem.InvoiceID.ToString(),
+                    lineItem.InvoiceDate.ToShortDateString(),
+                    lineItem.InvoiceTotal.ToString("c"),
+                    lineItem.ProductCode,
+                    lineItem.UnitPrice.ToString("c"),
+                    lineItem.Quantity.ToString(),
+                    lineItem.ItemTotal.ToString("c")
+                }));
+            }
+        }
+    }
+}
